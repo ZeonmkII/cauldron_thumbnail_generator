edbc6b6548d0219951c2e7767dba8b3dc4f16571
Mathus Tuachob <zeon_mk2@msn.com>
2023-02-20 18:07:46 +0700

    generate thumbnails

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
 use colored::*;
-// use neo4rs::*;
-// use image::imageops::FilterType;
-// use image::ImageError;
 use pathdiff::diff_paths;
-// use rayon::prelude::*;
-use std::fs::create_dir_all;
+use rayon::prelude::*;
 use std::io;
 use std::path::Path;
-use walkdir::{DirEntry, WalkDir};
-
-// [WalkDir] We'll skip the file if it is supposed to be hidden
-fn is_not_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| entry.depth() == 0 || !s.starts_with("."))
-        .unwrap_or(false)
-}
+use std::{error::Error, fs::create_dir_all};
+use walkdir::WalkDir;
+// use neo4rs::*;
 
 /*
     Path:
@@ -27,22 +16,22 @@ fn is_not_hidden(entry: &DirEntry) -> bool {
 
 // [Neo4J] Save Directory into Database
 fn add_directory_node(path: &Path) {
-    let _name: &str = path.file_name().unwrap().to_str().unwrap();
-    let _parent: &str = path.parent().unwrap().to_str().unwrap();
+    // let _name: &str = path.file_name().unwrap().to_str().unwrap();
+    // let _parent: &str = path.parent().unwrap().to_str().unwrap();
     let full_path: String = path.display().to_string();
 
     println!("DIR : {}", full_path.blue());
-    // TODO! Insert folder into Database
+    // TODO! #{1} Insert folder into Database
 }
 
 // [Neo4J] Save Filename into Database
 fn add_file_node(path: &Path) {
     let name: &str = path.file_name().unwrap().to_str().unwrap();
-    let _parent: &str = path.parent().unwrap().to_str().unwrap();
-    let _full_path: String = path.display().to_string();
+    // let _parent: &str = path.parent().unwrap().to_str().unwrap();
+    // let _full_path: String = path.display().to_string();
 
     println!("File: {}", name);
-    // TODO! Insert file into Database
+    // TODO! #{2} Insert file into Database
 }
 
 // Create Folder for the thumbnails
@@ -53,11 +42,13 @@ fn create_folder(path: &Path, root: &str, dest: &Path) -> std::io::Result<()> {
 }
 
 // Generate thumbnail for the image file
-fn create_thumbnail(path: &Path, root: &str, dest: &Path) -> std::io::Result<()> {
-    // TODO! Implement thumbnail generator function
-    // 1. check if file is image type
-    // 2. generate thumbnail according to size_input
-    Ok(())
+fn create_thumbnail(path: &Path, root: &str, dest: &Path) -> Result<(), Box<dyn Error>> {
+    let relative_path = diff_paths(path, &root);
+    let img = image::open(path)?;
+    // TODO #{4} Specify thumbnail size?
+    Ok(img
+        .thumbnail(200, 200)
+        .save(dest.join(relative_path.unwrap()))?)
 }
 
 fn main() {
@@ -67,10 +58,7 @@ fn main() {
     // Loop to get 'proper' directory input from user
     let lib_dir: String = loop {
         println!("{}", divider.purple().bold());
-        println!(
-            "Please input the {}: ",
-            "Image Library Directory".purple().bold()
-        );
+        println!("Please input the {}: ", "Library Directory".purple().bold());
         println!("(example: {} )", "D:\\path\\to\\folder".italic());
         println!("{}", divider.purple().bold());
 
@@ -86,15 +74,12 @@ fn main() {
         // Check if the input directory exist.
         if Path::new(&user_input).exists() {
             println!("{}", divider.green());
-            println!("The path seems {}! Continue...", "correct".green());
+            println!("The folder {}! Continue...", "exist".green());
             println!("{}", divider.green());
             break user_input;
         } else {
             println!("{}", divider.red());
-            println!(
-                "The folder {}! Please re-check and try again.",
-                "doesn't exist".red()
-            );
+            println!("The folder {}! Please re-check.", "doesn't exist".red());
             println!("{}", divider.red());
         }
     };
@@ -105,34 +90,37 @@ fn main() {
 
     // Library are original files, Destination is for the thumbnails
     let lib_root = lib_dir.clone();
-    // TODO! receive thumbnail path input from user?
+    // TODO! #{3} receive thumbnail path input from user?
     let dest_root = Path::new("D:\\Pictures\\thumbnail");
 
     // Traverse the Library directory, save structure to Database, and generate the thumbnails
     WalkDir::new(lib_dir)
         .into_iter()
-        .filter_entry(|e| is_not_hidden(e))
+        .par_bridge()
         .filter_map(|v| v.ok())
         .for_each(|x| {
-            // Is it a File or Folder?
             if x.path().is_dir() {
-                // Folder: create empty folder, keeping the same directory structure
+                // Folder:
                 match create_folder(x.path(), &lib_root, dest_root) {
                     Ok(_value) => {
-                        add_directory_node(x.path()); // add to Database
+                        add_directory_node(x.path()); // to Database
                     }
                     Err(error) => {
-                        println!("Error: {}", error.to_string());
+                        println!("ERROR: {}", error.to_string());
                     }
                 }
             } else {
-                // File: generate thumbnail for image file, into the prepared folder
+                // File:
                 match create_thumbnail(x.path(), &lib_root, dest_root) {
                     Ok(_value) => {
-                        add_file_node(x.path()); // add to Database
+                        add_file_node(x.path()); // to Database
                     }
                     Err(error) => {
-                        println!("Error: {}", error.to_string());
+                        println!(
+                            "ERROR:{} : {}",
+                            x.path().file_name().unwrap().to_str().unwrap().red(),
+                            error.to_string()
+                        );
                     }
                 }
             }
@@ -140,7 +128,17 @@ fn main() {
 
     // RunTime Logger
     let elapsed = now.elapsed();
-    println!("{}", divider.white().bold());
-    println!("    The Process took: {:.2?} to completed.", elapsed);
-    println!("{}", divider.white().bold());
+    println!("{}", divider.purple().bold());
+    println!(
+        "    Process took: {} seconds to complete.",
+        elapsed.as_secs().to_string().red()
+    );
+    println!("{}", divider.purple().bold());
 }
+
+// TODO! List
+// TODO! #{1} save folder structure into Database
+// TODO! #{2} save (only image) file into Database
+// TODO! #{3} receive user input for Thumbnail directory
+// TODO  #{4} let user specify thumbnail sizes (as options; s:150px, m:200px, l:250px, etc.)
+// TODO  #{5} refactor user input parts (both Library and Thumbnail) into its own function
